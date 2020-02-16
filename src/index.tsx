98877c6b060a0ae1f901ca51adbe8fcbe47024d4
yyamada12 <12yacropolisy@gmail.com>
2020-02-16 16:53:25 +0900

    add props to Square

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+interface SquarePropsInterface {
+    value: number;
+}
+class Square extends React.Component<SquarePropsInterface> {
    render() {
        return (
            <button className="square">
-               {/* TODO */}
+               {this.props.value}
            </button>
        );
    }
@@ -14,7 +17,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
    renderSquare(i: number) {
-       return <Square />;
+       return <Square value={i} />;
    }
 
    render() {
